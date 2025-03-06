2d67ba16b7cdf695114d18519a4bd4bbb09b9605
boolforge <josefranciscocarrascobenitez@gmail.com>
2025-03-06 21:33:06 +0100

    Update detection.h

--- detection.h
+++ detection.h
@@ -1,22 +1,7 @@
-/* ScummVM - Graphic Adventure Engine
+/* ScummVM - ACK Engine Detection Header
  *
- * ScummVM is the legal property of its developers, whose names
- * are too numerous to list here. Please refer to the COPYRIGHT
- * file distributed with this source distribution.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * Defines the interface for detecting ACK games.
+ * Integrated into ScummVM.
  */
 
 #ifndef ACK_DETECTION_H
@@ -26,15 +11,17 @@
 
 namespace Ack {
 
+// Debug channel definitions.
 enum {
-    kDebugGeneral = 1 << 0,
-    kDebugGraphics = 1 << 1,
-    kDebugIO = 1 << 2
+	kDebugGeneral = 1 << 0,
+	kDebugGraphics = 1 << 1,
+	kDebugIO = 1 << 2
 };
 
+// Structure for ACK game descriptions.
 struct AckGameDescription {
-    ADGameDescription desc;
-    int gameType;
+	ADGameDescription desc;
+	int gameType;
 };
 
 const char *getGameId(const AckGameDescription *gd);
